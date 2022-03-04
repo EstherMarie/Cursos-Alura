9e52db18f419a9907929757dcbd8fc3f376626ad
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-03-03 22:00:31 -0300

    TypeScript Parte 1 | Aula 1
    - Introdução ao projeto
    - Modelagem de uma Negociação em Javascript

--- TypeScript-parte-1/dist/js/models/negociacao.js
+++ TypeScript-parte-1/dist/js/models/negociacao.js
@@ -1,14 +1,31 @@
+// * Não pode ser modificada depois de criada.
+// * Obrigatoriamente tem uma data, quantidade e valor.
+// * Seu volume é calculado multiplicando-se a quantidade negociada no dia pelo valor negociado.
+
 export class Negociacao {
-    constructor(_data, quantidade, valor) {
-        this._data = _data;
-        this.quantidade = quantidade;
-        this.valor = valor;
-    }
-    get volume() {
-        return this.quantidade * this.valor;
-    }
-    get data() {
-        const data = new Date(this._data.getTime());
-        return data;
-    }
+	#data;
+	#quantidade;
+	#valor;
+
+	constructor(data, quantidade, valor) {
+		this.#data = data;
+		this.#quantidade = quantidade;
+		this.#valor = valor;
+	}
+
+	get data() {
+		return this.#data;
+	}
+
+	get quantidade() {
+		return this.#quantidade;
+	}
+
+	get valor() {
+		return this.#valor;
+	}
+
+	get volume() {
+		return this.#quantidade * this.#valor;
+	}
 }
